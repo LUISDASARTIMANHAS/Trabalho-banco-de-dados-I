3fd80410782ef52477b55fe265c573ff8f095e0d
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-04-22 20:22:42 -0300

    updated sql

--- scripts/selects de teste das tabelas sistema de hospital.sql
+++ scripts/selects de teste das tabelas sistema de hospital.sql
@@ -1,11 +1,63 @@
--- select * from Receita;
--- select * from Diagnostico ;
--- select * from Plano_Paciente ;
--- select * from Medico_Especialidade ;
--- select * from Consulta ;
--- select * from Medico ;
--- select * from Paciente ;
--- select * from Medicamento ;
--- select * from Doenca ;
--- select * from Plano ;
--- select * from Especialidade ;
+-- select
+--     *
+-- from
+--     Receita;
+
+-- select
+--     *
+-- from
+--     Diagnostico;
+
+-- select
+--     *
+-- from
+--     Plano_Paciente;
+
+-- select
+--     *
+-- from
+--     Medico_Especialidade;
+
+-- select
+--     *
+-- from
+--     Consulta;
+
+-- select
+--     *
+-- from
+--     Medico;
+
+-- select
+--     *
+-- from
+--     Paciente;
+
+-- select
+--     *
+-- from
+--     Medicamento;
+
+-- select
+--     *
+-- from
+--     Doenca;
+
+-- select
+--     *
+-- from
+--     Plano;
+
+-- select
+--     *
+-- from
+--     Especialidade;
+
+
+select 
+	* 
+from paciente
+JOIN plano_paciente as plan
+                on paciente.idpaciente = plan.idPlano
+-- JOIN consulta ON consulta.idpaciente = paciente.idpaciente
+				
