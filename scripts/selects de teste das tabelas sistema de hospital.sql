6129b1bd9aa5e6d60efc233366c1fff4ce4a34bb
LUISDASARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-04-22 20:10:03 -0300

    updated sql

---
+++ scripts/selects de teste das tabelas sistema de hospital.sql
@@ -0,0 +1,11 @@
+-- select * from Receita;
+-- select * from Diagnostico ;
+-- select * from Plano_Paciente ;
+-- select * from Medico_Especialidade ;
+-- select * from Consulta ;
+-- select * from Medico ;
+-- select * from Paciente ;
+-- select * from Medicamento ;
+-- select * from Doenca ;
+-- select * from Plano ;
+-- select * from Especialidade ;
