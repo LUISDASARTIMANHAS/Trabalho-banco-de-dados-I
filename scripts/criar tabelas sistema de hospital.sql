5c734184869a25e1c082eaed2d24d42ddf7fbb50
LUIS_DAS_ARTIMANHAS <luisaugustodesouza785@gmail.com>
2025-04-23 22:26:28 -0300

    Update criar tabelas sistema de hospital.sql

--- scripts/criar tabelas sistema de hospital.sql
+++ scripts/criar tabelas sistema de hospital.sql
@@ -1,48 +1,110 @@
-CREATE TABLE
-    consulta (
-        cod_consulta SERIAL NOT NULL PRIMARY KEY,
-        cod_medico INT NOT NULL,
-        cod_paciente INT NOT NULL,
-        data date NULL
-    );
+CREATE TABLE Medicamento (
+    idMedicamento SERIAL NOT NULL PRIMARY KEY,
+    nomeMedicamento VARCHAR(100) NOT NULL
+);
 
-CREATE TABLE
-    paciente (
-        cod_paciente SERIAL NOT NULL PRIMARY KEY,
-        nome varchar(255) NOT NULL,
-        cpf char(14) NOT NULL
-    );
+CREATE TABLE Doenca (
+    idDoenca SERIAL NOT NULL PRIMARY KEY,
+    nomeDoenca VARCHAR(100) NOT NULL
+);
 
-CREATE TABLE
-    medico (
-        cod_medico SERIAL NOT NULL PRIMARY KEY,
-        CRM char(10) NOT NULL,
-        nome varchar(255) NOT NULL
-    );
+CREATE TABLE Plano (
+    idPlano SERIAL NOT NULL PRIMARY KEY,
+    nomePlano VARCHAR(100) NOT NULL
+);
 
-CREATE TABLE
-    especialidade (
-        cod_especialidade SERIAL NOT NULL PRIMARY KEY,
-        tipo varchar(255) NOT NULL
-    );
+CREATE TABLE Paciente (
+    idPaciente SERIAL NOT NULL PRIMARY KEY,
+    nomePaciente VARCHAR(100) NOT NULL,
+    CPF VARCHAR(14) NOT NULL,
+    telefone VARCHAR(15) NOT NULL
+);
 
-CREATE TABLE
-    plano (
-        cod_plano SERIAL NOT NULL PRIMARY KEY,
-        nome varchar(255) NOT NULL
-    );
+CREATE TABLE Medico (
+    idMedico SERIAL NOT NULL PRIMARY KEY,
+    nomeMedico VARCHAR(100) NOT NULL,
+    CRM VARCHAR(11) NOT NULL
+);
 
-CREATE TABLE
-    medicamento (
-        cod_medicamento SERIAL NOT NULL PRIMARY KEY,
-        nome varchar(255) NOT NULL
-    );
+CREATE TABLE Especialidade (
+    idEspecialidade SERIAL NOT NULL PRIMARY KEY,
+    nomeEspecialidade VARCHAR(100) NOT NULL
+);
 
-CREATE TABLE
-    doenca (
-        cod_doenca SERIAL NOT NULL PRIMARY KEY,
-        nome varchar(255) NOT NULL
-    );
+CREATE TABLE Consulta (
+    idConsulta SERIAL NOT NULL PRIMARY KEY,
+    idMedico INT NOT NULL,
+    idPaciente INT NOT NULL,
+    dataConsulta DATE NOT NULL,
+	
+    FOREIGN KEY (idMedico) 
+		REFERENCES Medico(idMedico) 
+			ON DELETE RESTRICT,
+			
+    FOREIGN KEY (idPaciente) 
+		REFERENCES Paciente(idPaciente) 
+			ON DELETE RESTRICT
+);
 
-ALTER TABLE
--- DROP TABLE consulta, doenca,especialidade,medicamento,medico,paciente,plano;
+CREATE TABLE Receita (
+    idConsulta INT NOT NULL,
+    idMedicamento INT NOT NULL,
+    dose VARCHAR(50) NOT NULL,
+    intervaloDose VARCHAR(50) NOT NULL,
+	
+    PRIMARY KEY (idConsulta, idMedicamento),
+	
+    FOREIGN KEY (idConsulta) 
+		REFERENCES Consulta(idConsulta) 
+			ON DELETE RESTRICT,
+			
+    FOREIGN KEY (idMedicamento) 
+		REFERENCES Medicamento(idMedicamento) 
+			ON DELETE RESTRICT
+);
+
+CREATE TABLE Diagnostico (
+    idConsulta INT NOT NULL,
+    idDoenca INT NOT NULL,
+    descricao VARCHAR(255) NOT NULL,
+	
+    PRIMARY KEY (idConsulta, idDoenca),
+	
+    FOREIGN KEY (idConsulta) 
+		REFERENCES Consulta(idConsulta) 
+			ON DELETE RESTRICT,
+	
+    FOREIGN KEY (idDoenca) 
+		REFERENCES Doenca(idDoenca) 
+			ON DELETE RESTRICT
+);
+
+CREATE TABLE Plano_Paciente (
+    idPlano INT NOT NULL,
+    idPaciente INT NOT NULL,
+	
+    PRIMARY KEY (idPlano, idPaciente),
+	
+    FOREIGN KEY (idPlano) 
+		REFERENCES Plano(idPlano) 
+			ON DELETE RESTRICT,
+			
+    FOREIGN KEY (idPaciente) 
+		REFERENCES Paciente(idPaciente) 
+			ON DELETE RESTRICT
+);
+
+CREATE TABLE Medico_Especialidade (
+    idMedico INT NOT NULL,
+    idEspecialidade INT NOT NULL,
+	
+    PRIMARY KEY (idMedico, idEspecialidade),
+	
+    FOREIGN KEY (idMedico) 
+		REFERENCES Medico(idMedico) 
+			ON DELETE RESTRICT,
+			
+    FOREIGN KEY (idEspecialidade) 
+		REFERENCES Especialidade(idEspecialidade) 
+			ON DELETE RESTRICT
+);
